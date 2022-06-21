05badfaca094a0b025b221b6716edaa5b3da787f
Samuel Gutierrez <s.gutierrezj@uniandes.edu.co>
2022-06-21 13:55:31 -0500

    learning how to designate an object and functions to get their attributes and classes

---
+++ intro a objetos 1.R
@@ -0,0 +1,84 @@
+x=1
+x
+print(x)
+msg <- "hello"
+print(msg)
+x <- 10:30
+print(x)
+length(x)
+median(x)
+mean(x)
+sum(x)
+sum(x)/length(x)
+420/21
+y = 2:6
+y
+sum(y+x)
+sum(y)+sum(x)
+sum(y)
+matrix(c(x, y), nrow = 1, ncol = 2)
+x=
+x
+x
+x = c(1:Inf)
+x = 1:Inf
+x
+sum(2x+1)
+sum(x)
+NaN
+var(x)
+1L
+1
+x= 10:40
+x
+length(x)
+attributes(x)
+x = c(10:40)
+x
+attributes(x)
+class(x)
+`attributes<-`(x)
+attributes(x)
+names(x)
+dimnames(x)
+className(x)
+y=(T, F)
+Y <- C(TRUE)
+y <- c(TRUE)
+Y
+y
+print(y)
+y = c(TRUE, FALSE)
+y
+w <- c(0.866, 24.984)
+print(w)
+length(w)
+class(w)
+e <- c("a", "c", 32, 45)
+e
+as.numeric(e)
+as.numeric(e)
+as.logical(e)
+as.character(e)
+y <- matrix(1:25, nrow = 5, ncol = 5)
+y
+z <- 1:5
+x <- 6:10
+c <- 11-15
+v <- 16-20
+b <- 21-25
+length(c)
+c <- 11:15
+v <- 16-20
+v <- 16:20
+b <- 21:25
+length(c)
+v <- 16:20
+y <- rbind(z, x, c, v, b)
+print(y)
+x = matrix(1:25, nrow = 5, ncol = 5)
+print(x)
+x*y
+z <- x*y
+length(z)
+dim(z)
